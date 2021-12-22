6f50e973617efbe9a0946c95714e4c7374e9faaa
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-22 09:04:48 +0330

    modified the port

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -21,7 +21,7 @@ func init() {
 	// we have configured this database to use utf we are gonna place the charset = utf
 	// username:password@tcp(host)/
 	// this is the schema we have to use : "%s:%s@tcp(%s)/%s?charset=utf8"
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "1234", "127.0.0.1", "users_db")
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "1234", "127.0.0.1:3306", "users_db")
 	var err error
 	Client, err := sql.Open("mysql", dataSourceName)
 
