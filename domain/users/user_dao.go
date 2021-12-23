c878ee4b058f677c954348d22f20151b8b5bdab2
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-23 13:06:17 +0330

    handling mysql errors

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -6,19 +6,16 @@
 package users
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/delaram-gholampoor-sagha/bookstore-users-api/datasources/mysql/users_db"
 	"github.com/delaram-gholampoor-sagha/bookstore-users-api/utils/date_utils"
 	"github.com/delaram-gholampoor-sagha/bookstore-users-api/utils/errors"
+	"github.com/delaram-gholampoor-sagha/bookstore-users-api/utils/mysql_utils"
 )
 
 const (
 	indexUniqueEmail = "email_UNIQUE"
 	queryInsertUser  = "INSERT INTO users(first_name , last_name , email , date_created) VALUES(?, ?, ?, ?);"
 	GetUser          = "SELECT id , first_name , last_name , email , date_created  FROM users WHERE id = ?"
-	errorNoRows      = "no rows in result set"
 )
 
 // when we say get we get a user by its id (primary key)
@@ -31,11 +28,9 @@ func (user *User) Get() *errors.RestErr {
 
 	result := stmt.QueryRow()
 	// take what ever you have as an id in the database and use that value to populate these fields
-	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
-		if strings.Contains(err.Error(), errorNoRows) {
-			return errors.NewNotFoundError(fmt.Sprintf("user %d not found  ", user.Id))
-		}
-		return errors.NewIntervalServerError(fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
+	if getErr := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); getErr != nil {
+
+		return mysql_utils.ParseError(getErr)
 
 	}
 
@@ -53,18 +48,14 @@ func (user *User) Save() *errors.RestErr {
 
 	user.DateCreated = date_utils.GetNowString()
 
-	insertResult, err := stmt.Exec(queryInsertUser, user.FirstName, user.LastName, user.Email, user.DateCreated)
-	if err != nil {
-		if strings.Contains(err.Error(), indexUniqueEmail) {
-			return errors.NewBadRequessrError(fmt.Sprintf("email %s already exists", user.Email))
-		}
-		return errors.NewIntervalServerError(fmt.Sprintf("error when trying to save user : %s ", err.Error()))
-
+	insertResult, saveErr := stmt.Exec(queryInsertUser, user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if saveErr != nil {
+		return mysql_utils.ParseError(saveErr)
 	}
 
 	userId, err := insertResult.LastInsertId()
 	if err != nil {
-		return errors.NewIntervalServerError(fmt.Sprintf("error when trying to save user : %s", err.Error()))
+		return mysql_utils.ParseError(err)
 	}
 
 	user.Id = userId
---
+++ utils/mysql_utils/mysql_utils.go
@@ -0,0 +1,31 @@
+package mysql_utils
+
+import (
+	"strings"
+
+	"github.com/delaram-gholampoor-sagha/bookstore-users-api/utils/errors"
+	"github.com/go-sql-driver/mysql"
+)
+
+const (
+	errorNoRows = "no rows in result set"
+)
+
+func ParseError(err error) *errors.RestErr {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	// if we werent able to convert the error we are getting from this function to the mysql error ...
+	if !ok {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError("no recors matching the given id ")
+		}
+		return errors.NewIntervalServerError("error parsing database response")
+	}
+
+	switch sqlErr.Number {
+	case 1062:
+		return errors.NewBadRequessrError("invalid data")
+	}
+
+	return errors.NewIntervalServerError("error processing request")
+
+}
